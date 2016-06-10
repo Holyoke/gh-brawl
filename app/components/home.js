2c9687f5162719ffa2250c3b8f0244ae194f65a6
Holyoke <kid.idioteque@gmail.com>
2016-06-09 19:08:43 -0700

    Setup PromptContainer, add context with router

--- app/components/home.js
+++ app/components/home.js
@@ -1,11 +1,19 @@
-var React = require('react');
+var React = require('react'),
+    transparentBg = require('../styles').transparentBg,
+    Link = require('react-router').Link;
 
 var Home = React.createClass({
   render: function () {
     return (
-      <div> Hello from Home! </div>
+      <div className='jumbotron col-sm-12 text-center' style={transparentBg}>
+        <h1>Github Brawl</h1>
+        <p className='lead'>Crazy minecraft saying</p>
+        <Link to='/playerOne'>
+          <button type='button' className='btn btn-lg btn-successs'>Begin</button>
+        </Link>
+      </div>
     )
   }
-})
+});
 
 module.exports = Home;
