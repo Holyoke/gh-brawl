c811c2aa8519e0b94f1021d8128d322c6298bb6b
Holyoke <kid.idioteque@gmail.com>
2016-07-22 17:51:10 -0700

    Add Try Again button

--- app/components/Results.js
+++ app/components/Results.js
@@ -1,13 +1,46 @@
 var React = require('react'),
-    PropTypes = React.PropTypes;
+    PropTypes = React.PropTypes,
+    styles = require('../styles/index'),
+    UserDetails = require('./UserDetails'),
+    UserDetailsWrapper = require('./UserDetailsWrapper'),
+    Link = require('react-router').Link;
 
-function puke(obj) {
-  return <pre>{JSON.stringify(obj, null, ' ')}</pre>
+function StartOver(props) {
+  return(
+    <div className='col-sm-12' styles={styles.space}>
+      <Link to='/playerOne'>
+        <button type='button' className='btn btn-lg btn-danger'>Try Again?</button>
+      </Link>
+    </div>
+  )
 }
 
 function Results (props) {
+  if (props.scores[0] === props.scores[1]) {
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBG}>
+      <h1>Draw!</h1>
+      <StartOver/>
+    </div>
+  }
+
+  var winningIdx = props.scores[0] > props.scores[1] ? 0 : 1;
+  var losingIdx = winningIdx === 0 ? 1 : 0;
+
   return (
-    <div>Results: {puke(props)}</div>
+    <div className='jumbotron col-sm-12 text-center' style={styles.transparentBG}>
+      <h1>Results</h1>
+      <div className='col-sm-8 col-sm-offset-2'>
+        <UserDetailsWrapper header="Winner">
+          <UserDetails score={props.scores[winningIdx]} info={props.playersInfo[winningIdx]} />
+        </UserDetailsWrapper>
+
+        <UserDetailsWrapper header="Loser">
+          <UserDetails score={props.scores[losingIdx]} info={props.playersInfo[losingIdx]} />
+        </UserDetailsWrapper>
+      </div>
+
+      <StartOver/>
+    </div>
   )
 }
 
